0f64cd9925df596af461d6629720c59cd272452e
yashh_jdv <yashj98819@gmail.com>
2025-04-08 11:57:47 +0530

    code added in Emp

--- EmpJDBCProject/src/com/hexaware/entity/Emp.java
+++ EmpJDBCProject/src/com/hexaware/entity/Emp.java
@@ -1,5 +1,21 @@
 package com.hexaware.entity;
 
-public class Emp{
-    //write code here
+public class Emp {
+    private int EmpId;
+    private String EmpName;
+    public int getEmpId() {
+        return EmpId;
+    }
+    public void setEmpId(int empId) {
+        EmpId = empId;
+    }
+    public String getEmpName() {
+        return EmpName;
+    }
+    public void setEmpName(String empName) {
+        EmpName = empName;
+    }
+
+
+
 }
