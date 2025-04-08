34ba58b6f87e4049672f6f7b9ba2a0dda337e1c9
Udhaya58 <udhayamoorthy58@gmail.com>
2025-04-08 12:45:16 +0530

    Exception added

--- EmpJDBCProject/src/com/hexaware/exception/EmpNotFoundException.java
+++ EmpJDBCProject/src/com/hexaware/exception/EmpNotFoundException.java
@@ -1,9 +1,31 @@
-package com.hexaware.Exception;
+package com.hexaware.exception;
 
-public class EmpNotFoundException{
+public class EmpNotFoundException extends Exception{
     public EmpNotFoundException() {
     }
 
+	public EmpNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmpNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmpNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmpNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+    
+    
     //add constructor
 
     //yash -- test01
